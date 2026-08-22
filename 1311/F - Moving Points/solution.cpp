ec1f53d58fa4d635e2f8f53b77f83133dbba0365
Vansh Singhal <vsinghal0528@gmail.com>
2026-08-22 22:35:56 +0530

    Add Moving Points [F] from Codeforces

---
+++ 1311/F - Moving Points/solution.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+namespace __DEBUG_UTIL__ { void print(const char *x) { cerr << x; } void print(bool x) { cerr << (x ? "T" : "F"); } void print(char x) { cerr << '\'' << x << '\''; } void print(signed short int x) { cerr << x; } void print(unsigned short int x) { cerr << x; } void print(signed int x) { cerr << x; } void print(unsigned int x) { cerr << x; } void print(signed long int x) { cerr << x; } void print(unsigned long int x) { cerr << x; } void print(signed long long int x) { cerr << x; } void print(unsigned long long int x) { cerr << x; } void print(float x) { cerr << x; } void print(double x) { cerr << x; } void print(long double x) { cerr << x; } void print(string x) { cerr << '\"' << x << '\"'; } template <size_t N> void print(bitset<N> x) { cerr << x; } void print(vector<bool> v) { int f = 0; cerr << '{'; for (auto &&i : v) cerr << (f++ ? "," : "") << (i ? "T" : "F"); cerr << "}"; } template <typename T> void print(T &&x); template <typename T> void print(vector<vector<T>> mat); template <typename T, size_t N, size_t M> void print(T (&mat)[N][M]); template <typename F, typename S> void print(pair<F, S> x); template <typename T, size_t N> struct Tuple; template <typename T> struct Tuple<T, 1>; template <typename... Args> void print(tuple<Args...> t); template <typename... T> void print(priority_queue<T...> pq); template <typename T> void print(stack<T> st); template <typename T> void print(queue<T> q); template <typename T> void print(T &&x) { int f = 0; cerr << '{'; for (auto &&i : x) cerr << (f++ ? "," : ""), print(i); cerr << "}"; } template <typename T> void print(vector<vector<T>> mat) { int f = 0; cerr << "
+~~~~~
+"; for (auto &&i : mat) { cerr << setw(2) << left << f++, print(i), cerr << "
+"; } cerr << "~~~~~
+"; } template <typename T, size_t N, size_t M> void print(T (&mat)[N][M]) { int f = 0; cerr << "
+~~~~~
+"; for (auto &&i : mat) { cerr << setw(2) << left << f++, print(i), cerr << "
+"; } cerr << "~~~~~
+"; } template <typename F, typename S> void print(pair<F, S> x) { cerr << '('; print(x.first); cerr << ','; print(x.second); cerr << ')'; } template <typename T, size_t N> struct Tuple { static void printTuple(T t) { Tuple<T, N - 1>::printTuple(t); cerr << ",", print(get<N - 1>(t)); } }; template <typename T> struct Tuple<T, 1> { static void printTuple(T t) { print(get<0>(t)); } }; template <typename... Args> void print(tuple<Args...> t) { cerr << "("; Tuple<decltype(t), sizeof...(Args)>::printTuple(t); cerr << ")"; } template <typename... T> void print(priority_queue<T...> pq) { int f = 0; cerr << '{'; while (!pq.empty()) cerr << (f++ ? "," : ""), print(pq.top()), pq.pop(); cerr << "}"; } template <typename T> void print(stack<T> st) { int f = 0; cerr << '{'; while (!st.empty()) cerr << (f++ ? "," : ""), print(st.top()), st.pop(); cerr << "}"; } template <typename T> void print(queue<T> q) { int f = 0; cerr << '{'; while (!q.empty()) cerr << (f++ ? "," : ""), print(q.front()), q.pop(); cerr << "}"; } void printer(const char *) {} template <typename T, typename... V> void printer(const char *names, T &&head, V &&...tail) { int i = 0; for (int bracket = 0; names[i] != '\0' and (names[i] != ',' or bracket > 0); i++) if (names[i] == '(' or names[i] == '<' or names[i] == '{') bracket++; else if (names[i] == ')' or names[i] == '>' or names[i] == '}') bracket--; cerr.write(names, i) << " = "; print(head); if (sizeof...(tail)) cerr << " ||", printer(names + i + 1, tail...); else cerr << "]
+"; } }
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << __LINE__ << ": [", __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#endif
+ 
+#define int long long
+#define ll long long
+#define pb push_back
+#define all(v) v.begin(), v.end()
+ 
+int M = 1e9 + 7;
+ 
+int n;
+vector<ll> bit1;
+vector<ll> bit2;
+map<int,int> mp;
+vector<int> vals;
+ 
+int query1(int x) {
+    int sum = 0;
+    while (x>0) {
+        sum += bit1[x];
+        x -= x&(-x);
+    }
+    return sum;
+}
+ 
+void update1(int x, int val) {
+    while (x<=n) {
+        bit1[x] += val;
+        x += x&(-x);
+    }
+}
+ 
+int query2(int x) {
+    int sum = 0;
+    while (x>0) {
+        sum += bit2[x];
+        x -= x&(-x);
+    }
+    return sum;
+}
+ 
+void update2(int x, int val) {
+    while (x<=n) {
+        bit2[x] += val;
+        x += x&(-x);
+    }
+}
+ 
+void solve(){
+    cin>>n;
+ 
+    vector<int> x(n);
+    vector<int> v(n);
+ 
+    bit1.assign(n+1,0);
+    bit2.assign(n+1,0);
+ 
+    set<int> st;
+ 
+    for(int i=0;i<n;i++) {
+        cin>>x[i];
+    }
+ 
+    for(int i=0;i<n;i++) {
+        cin>>v[i];
+        st.insert(v[i]);
+    }
+    int i=1;
+ 
+    vals.pb(0);
+    for (auto ele: st) {
+        mp[ele] = i;
+        i++;
+        vals.pb(ele);
+    }
+ 
+    // debug(mp);
+ 
+    vector<pair<int,int>> data(n);
+    for (int i=0; i<n;i++) {
+        data[i] = {x[i],v[i]};
+    }
+    sort(all(data));
+ 
+    ll ans = 0;
+ 
+    for (int i=0;i<n;i++) {
+        ans += query2(mp[data[i].second])*data[i].first - query1(mp[data[i].second]);
+ 
+        update1(mp[data[i].second],data[i].first);
+        update2(mp[data[i].second],1);
+    }
+ 
+    cout<<ans<<endl;
+}
+ 
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    long long t = 1;
+    // cin >> t;
+    while (t--){
+        solve();
+    }
+}
